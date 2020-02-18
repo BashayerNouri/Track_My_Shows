56fe9b9f4000598f487e43b83349fa92eb067f40
Bashayer Nouri <bashayer_nouri@hotmail.com>
2020-02-18 12:17:41 +0400

    finished searchbar functionality

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,15 +2,31 @@ import React, { Component } from "react";
 
 //Components
 import ShowsList from "./ShowsList";
+import SearchBar from "./SearchBar";
 
 //Data
 import shows from "../Data/shows";
 
 class HomePage extends Component {
+    state = {
+        filteredShows: shows
+      };
+    
+      filterShows = query => {
+        query = query.toLowerCase();
+        let filteredShows = shows.filter(show => {
+          return `${show.name}`.toLowerCase().includes(query.toLowerCase());
+        });
+        this.setState({ filteredShows: filteredShows });
+      };
+    
   render() {
     return (
-      <div className="container-responsive">
-        <div className="col-12"><ShowsList shows={shows} /></div>
+        <div className="container-responsive">
+        <div className="col-12">
+          <SearchBar filterShows={this.filterShows} />
+          <ShowsList filteredShows={this.state.filteredShows} />
+        </div>
       </div>
     );
   }
--- src/Components/ShowsList.js
+++ src/Components/ShowsList.js
@@ -3,9 +3,11 @@ import React from "react";
 //Components
 import ShowCard from "./ShowCard";
 
-function ShowsList({ shows }) {
-  let showCards = shows.map(show => <ShowCard show={show} key={show.id} />);
-  return <div className="row">{showCards}</div>;
-}
+function ShowsList({ filteredShows }) {
+    let showCards = filteredShows.map(show => (
+      <ShowCard show={show} key={show.id} />
+    ));
+    return <div className="row">{showCards}</div>;
+  }
 
 export default ShowsList;
