e8dd3c1a82f9a916b408b91b28b5746af9ad686d
Bashayer Nouri <bashayer_nouri@hotmail.com>
2020-02-18 12:35:02 +0400

    finished show details functionality

---
+++ src/Components/ShowDetail.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+//Components
+import Loading from "./Loading";
+import Seasons from "./Seasons";
+
+class ShowDetail extends Component {
+    state = {
+      selectedShow: null,
+      loading: true
+    };
+  
+    componentDidMount() {
+      this.getShow(this.props.match.params.showID);
+    }
+  
+    getShow(showID) {
+      const show = this.props.shows.find(show => show.id === +showID);
+      if (show) {
+        this.setState({
+          selectedShow: show,
+          loading: false
+        });
+      }
+    }
+  
+    render() {
+      if (this.state.loading) return <Loading />;
+      else {
+        const show = this.state.selectedShow;
+        const showName = show.name;
+        return (
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col">
+                <h3>{showName}</h3>
+                <img
+                  src={show.image.original}
+                  className="img-thumbnail img-fluid"
+                  alt={showName}
+                />
+              </div>
+              <div className="col">
+                <h3>Summary:</h3>
+                <p>{show.summary}</p>
+                <h3>Seasons:</h3>
+                <Seasons seasons={show.seasons} />
+              </div>
+            </div>
+          </div>
+        );
+      }
+    }
+  }
+
+export default ShowDetail;
